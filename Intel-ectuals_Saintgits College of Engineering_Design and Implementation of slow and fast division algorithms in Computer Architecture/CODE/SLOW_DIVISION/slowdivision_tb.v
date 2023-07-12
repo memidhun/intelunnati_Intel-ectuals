aa0b0ac26bceac21ae05a2e6dc0b206c24149080
Midhun Mathew <midhun2002mm@gmail.com>
2023-07-13 01:04:18 +0530

    Added TEST BENCH FOR SLOW DIVISION

---
+++ Intel-ectuals_Saintgits College of Engineering_Design and Implementation of slow and fast division algorithms in Computer Architecture/CODE/SLOW_DIVISION/slowdivision_tb.v
@@ -0,0 +1,25 @@
+module slowdivision_tb;
+
+reg clk,rst,start;
+reg [3:0]X,Y;
+wire [3:0]quot,rem;
+wire valid;
+
+always #5 clk = ~clk;
+
+slowdivision inst (clk,rst,start,X,Y,valid,quot,rem);
+
+initial
+$monitor($time,"X=%d, Y=%d, valid=%d, quot=%d, rem=%d ",X,Y,valid,quot,rem);
+
+initial
+begin
+X=15;Y=8;clk=1'b1;rst=1'b0;start=1'b0;
+#10 rst = 1'b1;
+#10 start = 1'b1;
+#10 start = 1'b0;
+@valid
+#10 X=10;Y=2;start = 1'b1;
+#10 start = 1'b0;
+end      
+endmodule
